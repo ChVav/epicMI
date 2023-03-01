269d234d69572303f8ac8f0a0dc0fa6bfc6c326b
TatianaNazarenko <56540046+TatianaNazarenko@users.noreply.github.com>
2023-03-01 23:20:42 +0000

    Added R Documentation for unreliability_MI

--- R/functions.R
+++ R/functions.R
@@ -1,6 +1,148 @@
 load("./data/probesII_EPICv1.RData")
 load("./data/probesI_EPICv1.RData")
 
+#' @title
+#' Unreliability estimation and normalized mean intensities (MI) scores calculation
+#' for Infinium type II probes on the Illumina MethylationEPIC microarray v1.0
+#'
+#' @aliases unreliability_MI
+#'
+#' @description
+#' Method for estimating probes 'unreliability' scores by simulating the beta-value
+#' distribution influenced by technical noise on different levels of green and red
+#' intensities signals. High "unreliability" values are associated with
+#' low reproducibility and low repeatability of beta-values and are due to low MI level
+#' (which is also calculated within a given function).
+#'
+#' @param probes
+#' A dataframe of type II or type I probes (of specific version of Illumina
+#' Methylation BeadChip array) which should have at least 2 column:
+#' - 'probe' column (name of probes);
+#' - 'CHR' column (with chromosome annotation).
+#'
+#' In package embedded probesII_EPICv1 dataframe (for type II probes of Illumina
+#' MethylationEPIC BeadChip v1.0) and probesI_EPICv1 dataframe (for type I probes
+#' of Illumina MethylationEPIC BeadChip v1.0). These dataframes also have additional
+#' column 'removed_to_EPICv2', where "1" - indicated that probe of Illumina
+#' MethylationEPIC BeadChip v1.0 was removed to Illumina MethylationEPIC BeadChip v2.0,
+#' and "0" - was not.
+#'
+#' @param RGset
+#' An object of class \code{RGChannelSet}.
+#'
+#' @param noise_set
+#' Choice of a method of noise probes selection ('p' = default, 'Y'). 'p'-noise method -
+#' probes, which are failed (p-value > 0.01) on 50% samples, where p-values calculated
+#' by \code{detectionP} function (\code{minfi} package); Y-noise method - probes on the
+#' Y chromosome (for using only on female samples).
+#'
+#' @param samples
+#' Optional parameter of sample list (should be identical to IDs in RGset). Useful for using
+#' Y-noise method (for pre-selection of female samples). If missing, then all RGset IDs are
+#' used as a sample list.
+#'
+#' @param grid_max_intenisty
+#' Maximum value of intensity (green and red) of Reliability Map grid (5000 by default).
+#'
+#' @param grid_step
+#' Reliability Map grid step (of range from 0 to \code{grid_max_intenisty}) (100 by default).
+#'
+#' @param number_beta_generated
+#' The number of generated beta-values (with noise) that are used to estimate the range of
+#' their distribution (1000 by default).
+#'
+#' @return \code{probes} dataframe with 2 columns ('unreliability' and 'MI') added.
+#'
+#' @references
+#' Nazarenko T, Vavourakis CD, Jones A, Evans A, Watson AW, Brandt K,
+#' Carter C, Zaikin A, Herzog C, Widschwendter M.
+#' \emph{Technical and biological sources of unreliability of Infinium
+#' type II probes of the Illumina MethylationEPIC BeadChip microarray} (2023)
+#'
+#'
+#' @examples
+#' out <- unreliability_MI(probesII_EPICv1, RGset)
+#'
+#' out <- unreliability_MI(probesII_EPICv1, RGset, noise_set="Y", samples)
+#'
+#' @export unreliability_MI
+#'
+unreliability_MI <- function(probes, RGset, noise_set="p", samples, grid_max_intenisty = 5000, grid_step = 100, number_beta_generated = 1000) {
+  rownames(probes) <- probes$probe
+
+  if (!requireNamespace("minfi", quietly = TRUE)) stop("Package minfi must be installed to use this function.", call. = FALSE)
+
+  MSet <- minfi::preprocessRaw(RGset)
+  green_array <- minfi::getMeth(MSet)
+  red_array <- minfi::getUnmeth(MSet)
+
+  common_probes <- intersect(rownames(green_array), as.vector(probes$probe))
+  probes <- probes[common_probes, ]
+
+  if(missing(samples)) samples <- colnames(green_array)
+
+  now <- Sys.time()
+  print("Start...")
+  if(noise_set == "Y") {
+    noise_probes <- rownames(subset(probes, probes$CHR == "Y"))
+  } else {
+    if(noise_set == "p") {
+      print("p-value calculating...")
+      detP_rgset <- minfi::detectionP(RGset, type = "m+u")
+      pvalue_array <- data.frame(detP_rgset)
+      if(length(intersect(samples, colnames(pvalue_array))) != 0) {
+        countp_001 <- apply(pvalue_array[, samples], 1, function(x) sum(x > 0.01) / length(x))
+      }
+      if(length(intersect(paste0("X", samples), colnames(pvalue_array))) != 0) {
+        countp_001 <- apply(pvalue_array[, paste0("X", samples)], 1, function(x) sum(x > 0.01) / length(x))
+      }
+      noise_probes <- intersect(names(countp_001[countp_001 >= 0.5]), as.vector(probes$probe))
+    } else {
+      stop("Please, select CORRECT method of noise probes selection: p -- by p-value; Y -- by Y chromosomes probes (applicable only for female samples)", call. = FALSE)
+    }
+  }
+
+  if (length(intersect(samples, colnames(green_array))) == 0) stop("Loaded samples do not match by names with GREEN and RED arrays column names", call. = FALSE)
+
+  if (length(noise_probes) == 0) {
+    stop("0 noise probes were detected. Please, check you probes data frame (it should contain the maximum possible set of probes of the used array).", call. = FALSE)
+  } else {
+    print(str_c(length(noise_probes), " probes will be used for noise estimation"))
+  }
+
+  noise_probes <- intersect(noise_probes, as.vector(probes$probe))
+  noise_probes_df <- probes[noise_probes, ]
+  noise_probes_df$mean_green <- apply(green_array[noise_probes, samples], 1, function(x) mean(x, na.rm = T))
+  noise_probes_df$mean_red <- apply(red_array[noise_probes, samples], 1, function(x) mean(x, na.rm = T))
+
+  noise_probes_common_intensity <- noise_probes_df$mean_green + noise_probes_df$mean_red
+  names(noise_probes_common_intensity) <- noise_probes
+  selected_noise_probes <- names(noise_probes_common_intensity[noise_probes_common_intensity < 2 * density(noise_probes_common_intensity)$x[which.max(density(noise_probes_common_intensity)$y)]])
+
+  green_noise <- green_array[selected_noise_probes, samples]
+  red_noise <- red_array[selected_noise_probes, samples]
+
+  noise_matrix <- data.frame()
+  for(i in 1:length(selected_noise_probes)) {
+    temp <- rbind(data.frame(type="green", noise = as.vector(t(green_noise[selected_noise_probes[i], ]))),
+                  data.frame(type="red", noise = as.vector(t(red_noise[selected_noise_probes[i], ]))))
+    noise_matrix <- rbind(noise_matrix, temp)
+  }
+
+  unreliability_map <- unreliability_map_estimation(noise_matrix, grid_max_intenisty, grid_step, number_beta_generated)
+  print("Probes unreliability calculation ...")
+  unreliability <- unreliability_calculation(noise_matrix, samples, green_array, red_array, probes, unreliability_map, grid_max_intenisty, grid_step)
+  probes$unreliability <- unreliability
+
+  print("MI calculating...")
+  probes <- get_MI(probes, green_array, red_array, samples)
+
+  print("*Finish*")
+  print(str_c("Time: ", Sys.time() - now))
+  return(probes)
+}
+
+
 
 unreliability_map_estimation <- function(noise_matrix, grid_max_intenisty, grid_step, number_beta_generated) {
 
@@ -78,81 +220,6 @@ unreliability_calculation <- function(noise_matrix, samples,green_array, red_arr
 
   probes_unreliability<- apply(unreliability_array[2:ncol(unreliability_array)], 1, function(x) mean(x, na.rm=T))
   return(probes_unreliability)
-}
-
-unreliability_MI <- function(probes, RGset, noise_set="p", samples, grid_max_intenisty = 5000, grid_step = 100, number_beta_generated = 1000) {
-  rownames(probes) <- probes$probe
-
-  if (!requireNamespace("minfi", quietly = TRUE)) stop("Package minfi must be installed to use this function.", call. = FALSE)
-
-  MSet <- minfi::preprocessRaw(RGset)
-  green_array <- minfi::getMeth(MSet)
-  red_array <- minfi::getUnmeth(MSet)
-
-  common_probes <- intersect(rownames(green_array), as.vector(probes$probe))
-  probes <- probes[common_probes, ]
-
-  if(missing(samples)) samples <- colnames(green_array)
-
-  now <- Sys.time()
-  print("Start...")
-  if(noise_set == "Y") {
-    noise_probes <- rownames(subset(probes, probes$CHR == "Y"))
-  } else {
-    if(noise_set == "p") {
-      print("p-value calculating...")
-      detP_rgset <- minfi::detectionP(RGset, type = "m+u")
-      pvalue_array <- data.frame(detP_rgset)
-      if(length(intersect(samples, colnames(pvalue_array))) != 0) {
-        countp_001 <- apply(pvalue_array[, samples], 1, function(x) sum(x > 0.01) / length(x))
-      }
-      if(length(intersect(paste0("X", samples), colnames(pvalue_array))) != 0) {
-        countp_001 <- apply(pvalue_array[, paste0("X", samples)], 1, function(x) sum(x > 0.01) / length(x))
-      }
-      noise_probes <- intersect(names(countp_001[countp_001 >= 0.5]), as.vector(probes$probe))
-    } else {
-      stop("Please, select CORRECT method of noise probes selection: p -- by p-value; Y -- by Y chromosomes probes (applicable only for female samples)", call. = FALSE)
-    }
-  }
-
-  if (length(intersect(samples, colnames(green_array))) == 0) stop("Loaded samples do not match by names with GREEN and RED arrays column names", call. = FALSE)
-
-  if (length(noise_probes) == 0) {
-    stop("0 noise probes were detected. Please, check you probes data frame (it should contain the maximum possible set of probes of the used array).", call. = FALSE)
-    } else {
-      print(str_c(length(noise_probes), " probes will be used for noise estimation"))
-    }
-
-  noise_probes <- intersect(noise_probes, as.vector(probes$probe))
-  noise_probes_df <- probes[noise_probes, ]
-  noise_probes_df$mean_green <- apply(green_array[noise_probes, samples], 1, function(x) mean(x, na.rm = T))
-  noise_probes_df$mean_red <- apply(red_array[noise_probes, samples], 1, function(x) mean(x, na.rm = T))
-
-  noise_probes_common_intensity <- noise_probes_df$mean_green + noise_probes_df$mean_red
-  names(noise_probes_common_intensity) <- noise_probes
-  selected_noise_probes <- names(noise_probes_common_intensity[noise_probes_common_intensity < 2 * density(noise_probes_common_intensity)$x[which.max(density(noise_probes_common_intensity)$y)]])
-
-  green_noise <- green_array[selected_noise_probes, samples]
-  red_noise <- red_array[selected_noise_probes, samples]
-
-  noise_matrix <- data.frame()
-  for(i in 1:length(selected_noise_probes)) {
-    temp <- rbind(data.frame(type="green", noise = as.vector(t(green_noise[selected_noise_probes[i], ]))),
-                  data.frame(type="red", noise = as.vector(t(red_noise[selected_noise_probes[i], ]))))
-    noise_matrix <- rbind(noise_matrix, temp)
-  }
-
-  unreliability_map <- unreliability_map_estimation(noise_matrix, grid_max_intenisty, grid_step, number_beta_generated)
-  print("Probes unreliability calculation ...")
-  unreliability <- unreliability_calculation(noise_matrix, samples, green_array, red_array, probes, unreliability_map, grid_max_intenisty, grid_step)
-  probes$unreliability <- unreliability
-
-  print("MI calculating...")
-  probes <- get_MI(probes, green_array, red_array, samples)
-
-  print("*Finish*")
-  print(str_c("Time: ", Sys.time() - now))
-  return(probes)
 }
 
 get_MI <- function(probes, green_array, red_array, samples) {
